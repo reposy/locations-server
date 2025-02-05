a8944a2640880af0574cfe5164a1c22d05b4a39a
syhan <87705358+reposy@users.noreply.github.com>
2025-02-05 17:47:30 +0900

    UserSignInRequest.kt 메시지 수정

--- src/main/kotlin/springbootkotlin/sharemapserver/domain/auth/user/signin/api/req/UserSignInRequest.kt
+++ src/main/kotlin/springbootkotlin/sharemapserver/domain/auth/user/signin/api/req/UserSignInRequest.kt
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.NotBlank
 
 data class UserSignInRequest (
 
-    @field:NotBlank(message = "아이디는 Null 또는 빈 값이 아니어야 함")
+    @field:NotBlank(message = "아이디는 빈 값이 아니어야 합니다.")
     val username: String,
 
-    @field:NotBlank(message = "비밀번호는 Null 또는 빈 값이 아니어야 함")
+    @field:NotBlank(message = "비밀번호는 빈 값이 아니어야 합니다.")
     val password: String
 )
